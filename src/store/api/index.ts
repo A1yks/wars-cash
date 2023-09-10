c6d3ad4cb3f0c64460e6e9bf69fdbec5049f1d04
A1yks <kuftirev.alex@gmail.com>
2023-09-10 04:26:42 +0300

    changed baseUrl

--- src/store/api/index.ts
+++ src/store/api/index.ts
@@ -3,7 +3,7 @@ import { HYDRATE } from 'next-redux-wrapper';
 
 export const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
     extractRehydrationInfo(action, { reducerPath }) {
         if (action.type === HYDRATE) {
             return action.payload[reducerPath];
