16e144333d743f83da72a5cbfe43e503af6db9f0
A1yks <kuftirev.alex@gmail.com>
2023-09-10 03:45:47 +0300

    changed workflow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,19 +5,12 @@ module.exports = {
             script: 'npm',
             args: 'start',
             watch: false,
-            env: {
-                PORT: 80,
-            },
         },
         {
             name: 'wars-cash-staging',
             script: 'npm',
             args: 'start',
             watch: false,
-            env: {
-                NODE_ENV: 'staging',
-                PORT: 3000,
-            },
         },
     ],
 };
