1b7e1281ee16ce8bf203cd1273d9ab3251ce99de
A1yks <kuftirev.alex@gmail.com>
2023-10-10 02:00:33 +0300

    added fonts caching

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const withBundleAnalyzer = require('next-bundle-analyzer');
 
 const bundleAnalyzer = withBundleAnalyzer({ enabled: process.env.ANALYZE_BUNDLE === 'true' });
 
+const fontHeaders = [
+    {
+        key: 'Cache-control',
+        value: 'public, immutable, max-age=31536000',
+    },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
@@ -15,6 +22,14 @@ const nextConfig = {
         domains: ['localhost', '127.0.0.1', '116.202.113.54', 'wars.cash'],
     },
     distDir: 'dist/.next',
+    async headers() {
+        return [
+            {
+                source: '/:all*(ttf|otf|woff|woff2)',
+                headers: fontHeaders,
+            },
+        ];
+    },
 };
 
 module.exports = withPlugins([bundleAnalyzer], nextConfig);
